e9fd102a4c204f1e163b6cca53b55a4980c6975f
z1dos <georg.awdeew2017@yandex.ru>
2022-10-18 17:13:41 +0700

    еще что-то добавил

--- views/theme/view.php
+++ views/theme/view.php
@@ -1,47 +1,4 @@
 <?php
-
-use yii\helpers\Html;
-use yii\widgets\DetailView;
-
-/** @var yii\web\View $this */
-/** @var app\models\Theme $model */
-
-$this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Themes', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
-?>
-<div class="theme-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'text:ntext',
-            'status',
-            'date',
-            'id_user',
-        ],
-    ]) ?>
-
-</div>
-
-
-<?php
 //
 //use yii\helpers\Html;
 //use yii\widgets\DetailView;
@@ -58,15 +15,58 @@ $this->params['breadcrumbs'][] = $this->title;
 <!---->
 <!--    <h1>--><?//= Html::encode($this->title) ?><!--</h1>-->
 <!---->
+<!--    <p>-->
+<!--        --><?//= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+<!--        --><?//= Html::a('Delete', ['delete', 'id' => $model->id], [
+//            'class' => 'btn btn-danger',
+//            'data' => [
+//                'confirm' => 'Are you sure you want to delete this item?',
+//                'method' => 'post',
+//            ],
+//        ]) ?>
+<!--    </p>-->
+<!---->
 <!--    --><?//= DetailView::widget([
 //        'model' => $model,
-//        'attribute' => [
+//        'attributes' => [
+//            'id',
 //            'name',
-//            ['attribute' => 'date', 'format' => ['date', 'd-MM-Y H:i:s']],
-//            'text:ntext'
+//            'text:ntext',
+//            'status',
+//            'date',
+//            'id_user',
 //        ],
-//    ])
-//    ?>
+//    ]) ?>
 <!---->
 <!--</div>-->
+
+
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/** @var yii\web\View $this */
+/** @var app\models\Theme $model */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Themes', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="theme-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'name',
+            ['attribute' => 'date', 'format' => ['date', 'd-MM-Y H:i:s']],
+            'text:ntext'
+        ],
+    ])
+    ?>
+
+</div>
 
